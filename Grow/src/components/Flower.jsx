29ec800fedf34bcb8b09e4e5b5b343171be23c97
Saviho3 <saviodoraiswamy@gmail.com>
2025-08-28 23:24:26 -0600

    level up done for the most part

--- Grow/src/components/Flower.jsx
+++ Grow/src/components/Flower.jsx
@@ -1,19 +1,83 @@
-import React, { useState } from "react";
-import "./Grow.css";
+import React, { useState, useEffect } from "react";
 import {FaSpinner} from 'react-icons/fa';
 import { supabase } from '../supabaseClient.js'
 
-const Flower = () => {
+const Flower = ({taskTime, userLevel, currentExp, requiredExp, flowerType, onLevelUp }) => {
+  const userID = localStorage.getItem('user_id');
+  const [flowerFile, setFlowerFile] = useState(null);
 
-    const configureFlower = async () => {
+  const configureLevelup = async () => {
+    let newLevel = userLevel;
+    let newExp = currentExp;
+    let newRequiredExp = requiredExp;
 
+
+    if (currentExp >= requiredExp && requiredExp !== null &&  currentExp !== null) {
+      newLevel += 1;
+
+      if (newLevel >= 4 ) {
+        newLevel = 4;
+        newExp = 0;
+        newRequiredExp = 10;
+
+
+
+      } else {
+        newExp = currentExp - requiredExp;
+        newRequiredExp = requiredExp * 2;
+      }
+
+      onLevelUp(newLevel, newExp, newRequiredExp);
+      configureFlower();
+    }
+  };
+
+  const configureExp = async () => {
+    let ratio = taskTime/10;
+    let newExp  = ratio + currentExp;
+    if (newExp >= 0) {
+      onLevelUp(userLevel, newExp, requiredExp);
+    } else {
+      console.log(newExp);
+    }
+
+  }
+
+  useEffect(()=> {
+    if (taskTime !== null){
+    configureExp();
+    }
+  }, [taskTime])
+
+
+  useEffect(()=> {
+    configureLevelup()
+  }, [currentExp])
+
+  useEffect(() => {
+    configureFlower();
+  }, [userLevel, flowerType]);
+
+  const configureFlower = async () => {
+      if (userLevel < 4) {
+        if (userLevel == 1) {
+          setFlowerFile('../../images/level-'+userLevel+'.png')
+        } else {
+          setFlowerFile('../../images/level-'+userLevel+'.gif')
+        }
+      }
+      else {
+        setFlowerFile('../../images/'+flowerType+'.gif')
+      }
     }
 
 
 
-    return(
+  return(
         <div>
-            
+          {flowerFile && <img src={flowerFile} alt="image of flower" />}
+          <h3>Level: {userLevel}</h3>
+          <h3>Flower Growth Status: {currentExp}/{requiredExp} {userLevel}</h3>
         </div>
     )
 }
--- Grow/src/pages/App.jsx
+++ Grow/src/pages/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Login";
 import Grow from "./Grow";
+import FlowerSelect from './FlowerSelect';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/grow" element={<Grow />} />
+        <Route path="/flower-picker" element={<FlowerSelect />} />
         <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
